70c6b3499c56906b7f6c32fd0df53aca56838f57
ShaoEnChen <seanchen47@gmail.com>
2018-09-12 07:35:03 +0000

    finish part 1 & 2

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,33 +3,68 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  # sum = 0
+  # arr.each { |a| sum += a }
+  # return sum
+  arr.sum
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  if arr.length == 0
+    return 0
+  elsif arr.length == 1
+    return arr[0]
+  end
+  
+  first = -(2**(0.size * 8 -2))
+  second = -(2**(0.size * 8 -2))
+  arr.each { |a|
+    if a > first
+      second = first
+      first = a
+    elsif a > second
+      second = a
+    end
+  }
+  return first + second
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  if arr.length == 0
+    return false
+  end
+  h = { 0 => true }
+  arr.each { |a|
+    if h[n-a]
+      return true
+    end
+    h[a] = true
+  }
+  return false
 end
 
 # Part 2
 
-def hello(name)
-  # YOUR CODE HERE
+def hello name
+  return "Hello, " + name
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  if s.empty?
+    return false
+  end
+  return s =~ /^(?=[a-z])(?=[^aeiou])/i
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  if (s.length > 0) && (s.delete('01') == '') && (s.to_i(2) % 4 == 0)
+    return true
+  end
+  return false
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+
 end
